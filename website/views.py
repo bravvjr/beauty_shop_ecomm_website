74b7699cdd8a6d399cd7ea2b2d1081310c74b653
waweru89 <wawerumburu157@gmail.com>
2025-03-04 01:48:07 +0300

    changes

--- website/views.py
+++ website/views.py
@@ -150,28 +150,27 @@ def plus_cart():
 
         return jsonify(data)
 
-
 @views.route('/minuscart')
 @login_required
 def minus_cart():
     if request.method == 'GET':
         cart_id = request.args.get('cart_id')
         cart_item = Cart.query.get(cart_id)
-        cart_item.quantity = cart_item.quantity - 1
-        db.session.commit()
+
+        if cart_item and cart_item.quantity > 0:
+            cart_item.quantity -= 1
+            db.session.commit()
 
         cart = Cart.query.filter_by(customer_link=current_user.id).all()
-
-        amount = 0
-
-        for item in cart:
-            amount += item.product.current_price * item.quantity
+        amount = sum(item.product.current_price * item.quantity for item in cart)
 
         data = {
-            'quantity': cart_item.quantity,
+            'quantity': cart_item.quantity if cart_item else 0,
             'amount': amount,
             'total': amount + 200
         }
+        return jsonify(data)  # Ensure you return a response
+
 
         return jsonify(data)
 
